e53d5b638a462905d328a5cfd5e32113f4f75d0a
masterUNG <sukuma99@gmail.com>
2016-06-06 16:49:25 +0700

    ออกแบบ screen

--- app/src/main/java/pbru/it/sukuma/itpbru/MainActivity.java
+++ app/src/main/java/pbru/it/sukuma/itpbru/MainActivity.java
@@ -1,7 +1,9 @@
 package pbru.it.sukuma.itpbru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }// Main Method -- statment ;
+
+
+    public void clickSignUpMain(View view ) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+
     }
-}
+
+
+}   //main class นี่คือ คลาสหลัก
+
+
