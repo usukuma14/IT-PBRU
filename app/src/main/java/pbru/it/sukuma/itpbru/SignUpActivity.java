3fdf664a027fc19a387a15db8017f6457ad86cc8
masterUNG <sukuma99@gmail.com>
2016-06-07 13:59:00 +0700

    Post value to Server

--- app/src/main/java/pbru/it/sukuma/itpbru/SignUpActivity.java
+++ app/src/main/java/pbru/it/sukuma/itpbru/SignUpActivity.java
@@ -5,6 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //explicit ประกาศตัวแปร
@@ -39,7 +49,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         nameString = nameEditText.getText().toString().trim();
         surnameString = surnameEditText.getText().toString().trim();
-        userString = surnameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
 
@@ -57,12 +67,36 @@ public class SignUpActivity extends AppCompatActivity {
             //false
             uploadValueToServer();
 
-        }
+        }   //UPLOAD
 
 
     }   //click sign
 
     private void uploadValueToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlUpload).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+
+            }
+        });
 
     }
 
